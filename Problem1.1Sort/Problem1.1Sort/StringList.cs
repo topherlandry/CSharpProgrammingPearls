84548ca9c1960239361bad48b753bf0d6b61dbac
Topher Landry <topher.p.landry@gmail.com>
2013-04-23 21:54:05 -0400

    Problem 1.1
    Problem 1.1 specifies sorting in a language with libraries supporting
    sorts.
    
    This is incredibly easy, so I have made it more related to the problem
    posed in Column 1 which was a list of strings sorted with no duplicates.

---
+++ Problem1.1Sort/Problem1.1Sort/StringList.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem1._1Sort
+{
+    /// <summary>
+    /// Problem 1.1 - If memory were not scarce, how would you implement a sort
+    /// in a language with libraries for representing and sorting sets?
+    /// </summary>
+    public class StringList
+    {
+        public List<string> List { get; set; }
+
+        public StringList()
+        {
+            List = new List<string>();
+        }
+
+        /// <summary>
+        /// Return a string list containing no duplicates and sorted by string
+        /// comparison.
+        /// </summary>
+        /// <remarks>The problem called for a string list that did not contain
+        /// any duplicates.</remarks>
+        public List<string> GetDistinctSortedList()
+        {
+            return List.Distinct().OrderBy(s => s).ToList();
+        }
+    }
+}
