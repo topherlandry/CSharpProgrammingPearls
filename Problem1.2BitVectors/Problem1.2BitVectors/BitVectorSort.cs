baf8213206ab156bf11b6f6f416505a76857d433
Topher Landry <topher.p.landry@gmail.com>
2013-04-24 22:22:47 -0400

    Adding Solution for Problem 1.2
    Implemented using the BitArray .Net class instead of bitwise operators.
    BitArray seems like it was built expressly for the purpose of
    maintaining a bit vector.

---
+++ Problem1.2BitVectors/Problem1.2BitVectors/BitVectorSort.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1._2BitVectors
+{
+    /// <summary>
+    /// Class which reads in a list of ints of size n, and generates a bit 
+    /// vector to sort and store the results.
+    /// </summary>
+    public class BitVectorSort
+    {
+        private BitArray _vector;
+        private static bool SET_TO_NOT_FOUND = false;
+        private static bool SET_TO_FOUND = true;
+
+        public BitVectorSort(int size)
+        {
+            _vector = new BitArray(size, SET_TO_NOT_FOUND);
+        }
+
+        public BitArray Vector { get { return _vector; } }
+
+        public void AddCollectionToVector(IEnumerable<int> collection)
+        {
+            foreach (var number in collection)
+            {
+                ThrowIfOutOfRange(number);
+                _vector.Set(number, SET_TO_FOUND);
+            }
+        }
+
+        public bool IsSet(int number)
+        {
+            ThrowIfOutOfRange(number);
+            return _vector.Get(number);
+        }
+
+        public void Clear(int number)
+        {
+            ThrowIfOutOfRange(number);
+            _vector.Set(number, SET_TO_NOT_FOUND);
+        }
+
+        public void Set(int number)
+        {
+            ThrowIfOutOfRange(number);
+            _vector.Set(number, SET_TO_FOUND);
+        }
+
+        private void ThrowIfOutOfRange(int number)
+        {
+            string errorTemplate = "Index: {0} is out of bounds.";
+            var errorMessage = String.Format(errorTemplate, number.ToString());
+            if (number > _vector.Length)
+                throw new ArgumentException(errorMessage);
+        }
+    }
+}
---
+++ Problem1.2BitVectors/Problem1.2BitVectors/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1._2BitVectors
+{
+    /// <summary>
+    /// Problem 1.2 asks: How would you implement bit vectors using bitwise
+    /// logical operations (such as and, or and shift)?
+    /// 
+    /// In C#, I wouldn't. I'd use a BitArray since it would be easy to
+    /// check, set, and clear elements in the array. That's how this
+    /// solution was implemented. BitVectorSort implements the bit vector.
+    /// </summary>
+    public class Program
+    {
+        private static int MAX_SIZE = 1000;
+
+        static void Main(string[] args)
+        {
+            var randSeed = new Random();
+            var intList = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                intList.Add(randSeed.Next(MAX_SIZE));
+            }
+
+            var sort = new BitVectorSort(MAX_SIZE);
+            sort.AddCollectionToVector(intList);
+
+            for (int i = 0; i < MAX_SIZE; i++)
+            {
+                if (sort.IsSet(i))
+                    Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
