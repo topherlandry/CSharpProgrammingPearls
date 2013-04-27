df6e346292595d003fe82abe8096c1ee97e70440
Topher Landry <topher.p.landry@gmail.com>
2013-04-26 22:40:58 -0400

    Problem 1.3 Added.
    This problem compares the efficiency of the solutions for 1.1 and 1.2

---
+++ Problem1.3Efficiency/Problem1.3Efficiency/BitVectorSort.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Problem1._3Efficiency
+{
+    public class BitVectorSort
+    {
+        private BitArray _vector;
+        private static bool SET_TO_NOT_FOUND = false;
+        private static bool SET_TO_FOUND = true;
+
+        public BitVectorSort(int size)
+        {
+            _vector = new BitArray(size, SET_TO_NOT_FOUND);
+        }
+
+        public BitArray Vector { get { return _vector; } }
+
+        public void AddCollectionToVector(IEnumerable<int> collection)
+        {
+            foreach (var number in collection)
+            {
+                ThrowIfOutOfRange(number);
+                _vector.Set(number, SET_TO_FOUND);
+            }
+        }
+
+        public bool IsSet(int number)
+        {
+            ThrowIfOutOfRange(number);
+            return _vector.Get(number);
+        }
+
+        public void Clear(int number)
+        {
+            ThrowIfOutOfRange(number);
+            _vector.Set(number, SET_TO_NOT_FOUND);
+        }
+
+        public void Set(int number)
+        {
+            ThrowIfOutOfRange(number);
+            _vector.Set(number, SET_TO_FOUND);
+        }
+
+        private void ThrowIfOutOfRange(int number)
+        {
+            string errorTemplate = "Index: {0} is out of bounds.";
+            var errorMessage = String.Format(errorTemplate, number.ToString());
+            if (number > _vector.Length)
+                throw new ArgumentException(errorMessage);
+        }
+    }
+}
---
+++ Problem1.3Efficiency/Problem1.3Efficiency/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1._3Efficiency
+{
+    /// <summary>
+    /// Implement the bitmap sort on your system and measure its runtime;
+    /// how does it compare to the system sort and the sort in problem 1.1?
+    /// 
+    /// On my machine, the bit vector sort is consistently faster by
+    /// approximately 30% with this code.
+    /// </summary>
+    public class Program
+    {
+        // Problem called for list size of 1,000,000 with max int size of
+        // 10,000,000.
+        private const int LIST_SIZE = 1000000;
+        private const int MAX_INT = 10000000;
+
+        public static void Main()
+        {
+            var seed = new Random();
+            var unsortedList = new List<int>();
+
+            for(int i = 0;i < LIST_SIZE;i++)
+                unsortedList.Add(seed.Next(MAX_INT));
+
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            GetDistinctSortedList(unsortedList);
+            stopwatch.Start();
+
+            Console.WriteLine("System sort took {0} ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Reset();
+
+            var bitVector = new BitVectorSort(MAX_INT);
+
+            stopwatch.Start();
+            bitVector.AddCollectionToVector(unsortedList);
+            stopwatch.Stop();
+
+            Console.WriteLine("Bit vector sort took {0} ms.", stopwatch.ElapsedMilliseconds);
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Return a string list containing no duplicates and sorted by string
+        /// comparison.
+        /// </summary>
+        /// <remarks>The problem called for a string list that did not contain
+        /// any duplicates.</remarks>
+        public static List<int> GetDistinctSortedList(List<int> list)
+        {
+            return list.Distinct().OrderBy(s => s).ToList();
+        }
+    }
+}
