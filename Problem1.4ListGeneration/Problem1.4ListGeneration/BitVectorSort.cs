3d68b40fa80d7db2d8aa4de750327ff537a1ce41
Topher Landry <topher.p.landry@gmail.com>
2013-04-27 21:49:02 -0400

    Added Problem 1.4
    This problem was not easy. The random list generation started out being
    extremely inefficient, even if short. Finally settled on a very quick
    HashSet implementation.

---
+++ Problem1.4ListGeneration/Problem1.4ListGeneration/BitVectorSort.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Problem1._4ListGeneration
+{
+    public class BitVectorSort
+    {
+        private BitArray _vector;
+        private static bool SET_TO_NOT_FOUND = false;
+        private static bool SET_TO_FOUND = true;
+
+        public BitVectorSort(int size)
+        {
+            _vector = new BitArray(size, SET_TO_NOT_FOUND);
+        }
+
+        public BitArray Vector { get { return _vector; } }
+
+        public List<int> GetAsList()
+        {
+            var list = new List<int>();
+            for (int i = 0; i < _vector.Length; i++)
+                if (IsSet(i))
+                    list.Add(i);
+            return list;
+        }
+
+        /// <summary>
+        /// Returns the count of all bits set on.
+        /// </summary>
+        public int Count()
+        {
+            int count = 0;
+            for (int i = 0; i < _vector.Length; i++)
+                if (IsSet(i))
+                    count++;
+            return count;
+        }
+
+        public void AddCollectionToVector(IEnumerable<int> collection)
+        {
+            foreach (var number in collection)
+                Set(number);
+        }
+
+        public bool IsSet(int number)
+        {
+            ThrowIfOutOfRange(number);
+            return _vector.Get(number);
+        }
+
+        public void Clear(int number)
+        {
+            ThrowIfOutOfRange(number);
+            _vector.Set(number, SET_TO_NOT_FOUND);
+        }
+
+        public void Set(int number)
+        {
+            ThrowIfOutOfRange(number);
+            _vector.Set(number, SET_TO_FOUND);
+        }
+
+        private void ThrowIfOutOfRange(int number)
+        {
+            string errorTemplate = "Index: {0} is out of bounds.";
+            var errorMessage = String.Format(errorTemplate, number.ToString());
+            if (number > _vector.Length)
+                throw new ArgumentException(errorMessage);
+        }
+    }
+}
---
+++ Problem1.4ListGeneration/Problem1.4ListGeneration/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Problem1._4ListGeneration
+{
+    /// <summary>
+    /// Generate k integers less than n without duplicates.
+    /// In random order.
+    /// Short and efficient.
+    /// 
+    /// Perform same tests as in 1.3.
+    /// 
+    /// Results are still pointing in favor of the bit vector sort.
+    /// 
+    /// I implemented the random list generator in a number of different ways
+    /// before settling on the HashSet implementation. Every other way I
+    /// attempted to generate a distinct random list of m integers from n
+    /// possibilities was incredibly slow with m = 1,000,000 and
+    /// n = 10,000,000. This one executes fairly quickly.
+    /// </summary>
+    public class Program
+    {
+        // Problem called for list size of 1,000,000 with max int size of
+        // 10,000,000.
+        private const int LIST_SIZE = 1000000;
+        private const int MAX_INT = 10000000;
+
+        public static void Main()
+        {
+            var seed = new Random();
+            var unsortedList = GenerateRandomList(LIST_SIZE, MAX_INT);
+
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            GetDistinctSortedList(unsortedList);
+            stopwatch.Start();
+
+            Console.WriteLine("System sort took {0} ms.", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Reset();
+
+            var bitVector = new BitVectorSort(MAX_INT);
+
+            stopwatch.Start();
+            bitVector.AddCollectionToVector(unsortedList);
+            stopwatch.Stop();
+
+            Console.WriteLine("Bit vector sort took {0} ms.", stopwatch.ElapsedMilliseconds);
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Return a string list containing no duplicates and sorted by string
+        /// comparison.
+        /// </summary>
+        /// <remarks>The problem called for a string list that did not contain
+        /// any duplicates.</remarks>
+        public static List<int> GetDistinctSortedList(List<int> list)
+        {
+            return list.Distinct().OrderBy(s => s).ToList();
+        }
+
+        private static List<int> GenerateRandomList(int size, int maxValue)
+        {
+            var set = new HashSet<int>();
+            var seed = new Random();
+            while (set.Count < size)
+                set.Add(seed.Next(maxValue));
+
+            return set.ToList();
+        }
+    }
+}
